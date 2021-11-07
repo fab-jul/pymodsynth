b8daaf8268356d3758834bc17cf270f22aa75e0d
Fabian <fmentzer@me.com>
2021-11-07 09:25:19 +0100

    Clean up `gimme_sound`.py: remove global state, add some comments.

--- playground/gimme_sound.py
+++ playground/gimme_sound.py
@@ -15,6 +15,9 @@ import numpy as np
 import sounddevice as sd
 
 
+_COMMAND_QUEUE = queue.Queue()
+
+
 def int_or_str(text):
     """Helper function for argument parsing."""
     try:
@@ -23,55 +26,29 @@ def int_or_str(text):
         return text
 
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    'frequency', nargs='?', metavar='FREQUENCY', type=float, default=500,
-    help='frequency in Hz (default: %(default)s)')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='output device (numeric ID or substring)')
-parser.add_argument(
-    '-a', '--amplitude', type=float, default=0.2,
-    help='amplitude (default: %(default)s)')
-args = parser.parse_args(remaining)
-
-
-q = queue.Queue()
-
-
 class MakeSin:
-    def __init__(self, samplerate, freq):
-        self.samplerate = samplerate
-        self.freq = freq
+    def __init__(self, sample_rate, amplitude, frequency):
+        self.sample_rate = sample_rate
+        self.amplitude = amplitude
+        self.frequency = frequency
         self.i = 0
 
     def callback(self, outdata, frames, time, status):
         if status:
             print(status, file=sys.stderr)
-        t = (self.i + np.arange(frames)) / self.samplerate
+        t = (self.i + np.arange(frames)) / self.sample_rate
         t = t.reshape(-1, 1)
-        outdata[:] = args.amplitude * np.sin(2 * np.pi * self.freq * t)
+        outdata[:] = self.amplitude * np.sin(2 * np.pi * self.frequency * t)
         self.i += frames
 
     def up(self):
-        self.freq += 10
+        self.frequency += 10
 
     def down(self):
-        self.freq -= 10
+        self.frequency -= 10
 
 
-COMMANDS = {
+_COMMANDS = {
     "help": "Show help",
     "q": "Quit",
     "u": "Increase frequency",
@@ -81,48 +58,80 @@ COMMANDS = {
 
 def _print_help():
     print("Commands:")
-    for q, info in COMMANDS.items():
+    for q, info in _COMMANDS.items():
         print(q, ":", info)
 
 
-def gimme_sound():
-    t = threading.Thread(target=start_sound_thread)
+def gimme_sound(device, amplitude, frequency):
+    t = threading.Thread(target=start_sound, kwargs=dict(
+        device=device, amplitude=amplitude, frequency=frequency))
     t.start()
     print("Started. Type `help` to get started")
     while 1:
         try:
             print("Command:")
+            # This conveniently blocks this thread until input appears.
             i = input()
+            if i not in _COMMANDS:
+                print(f"Invalid command: `{i}`. Type `help` for help.")
+                continue
             if i == "help":
                 _print_help()
                 continue
-
-            q.put(i, block=False)
+            _COMMAND_QUEUE.put(i, block=False)
             if i == "q":
-                print("stopping...")
                 break
         except KeyboardInterrupt:
-            q.put("q")
+            _COMMAND_QUEUE.put("q")
             break
     sys.exit()
 
 
-def start_sound_thread():
-    samplerate = sd.query_devices(args.device, 'output')['default_samplerate']
-    sin = MakeSin(samplerate, freq=args.frequency)
+def start_sound(*, device, amplitude, frequency):
+    sample_rate = sd.query_devices(device, 'output')['default_samplerate']
+    sin = MakeSin(sample_rate, amplitude, frequency)
 
     with sd.OutputStream(
-            device=args.device, channels=1, callback=sin.callback, samplerate=samplerate):
+            device=device, channels=1, callback=sin.callback, samplerate=sample_rate):
         while 1:
-            entry = q.get(block=True)
-            if entry == "q":
+            command = _COMMAND_QUEUE.get(block=True)
+            if command == "q":
                 break
-            if entry == "u":
+            if command == "u":
                 sin.up()
-            elif entry == "d":
+            elif command == "d":
                 sin.down()
+            else:
+                print("Warning, unknown command:", command)
 
+
+def main():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        '-l', '--list-devices', action='store_true',
+        help='show list of audio devices and exit')
+    args, remaining = parser.parse_known_args()
+    if args.list_devices:
+        print(sd.query_devices())
+        sys.exit(0)
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        parents=[parser])
+    parser.add_argument(
+        'frequency', nargs='?', metavar='FREQUENCY', type=float, default=500,
+        help='frequency in Hz (default: %(default)s)')
+    parser.add_argument(
+        '-d', '--device', type=int_or_str,
+        help='output device (numeric ID or substring)')
+    parser.add_argument(
+        '-a', '--amplitude', type=float, default=0.2,
+        help='amplitude (default: %(default)s)')
+    args = parser.parse_args(remaining)
+
+    gimme_sound(args.device, amplitude=args.amplitude, frequency=args.frequency)
 
 
 if __name__ == "__main__":
-    gimme_sound()
+    main()
