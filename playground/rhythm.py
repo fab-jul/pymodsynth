ecb4fe2de80ebedc2fdc2f78f1b56bb0aab84ccd
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-11-28 19:26:20 +0100

    envelope generator API draft

--- playground/rhythm.py
+++ playground/rhythm.py
@@ -8,35 +8,45 @@ import matplotlib.pyplot as plt
 P = Parameter
 
 
-class ADSRShapeGen:
-    """Borrowed from modules.py"""
+class ADSREnvelopeGen:
+    """
+    Borrowed from modules.py
+    New api for envelope generators: They pass clock_signal to their param-sources, but only generate an envelope
+    for desired indices. Those are clear from the trigger signal of the calling function.
+    Therefore, __call__ takes a clock signal, a list of desired indices and returns a list of envelopes.
+    TODO: consider if we should enhance the Module.call signature with optional desired indices.
+    """
 
-    def __init__(self, attack, decay, sustain, release, hold):
+    def __init__(self, attack: Module, decay: Module, sustain: Module, release: Module, hold: Module):
         self.attack = attack
         self.decay = decay
         self.sustain = sustain
         self.release = release
         self.hold = hold
 
-    def __call__(self):
-        t_attack = round(self.attack.get())
-        t_decay = round(self.decay.get())
-        sustain_height = self.sustain.get()
-        t_hold = round(self.hold.get())
-        t_release = round(self.release.get())
+    def __call__(self, clock_signal: ClockSignal, desired_indices):
+        t_attack = self.attack(clock_signal)
+        t_decay = self.decay(clock_signal)
+        sustain_height = self.sustain(clock_signal)
+        t_hold = self.hold(clock_signal)
+        t_release = self.release(clock_signal)
 
-        attack = np.linspace(0, 1, t_attack)
-        decay = np.linspace(1, sustain_height, t_decay)
-        hold = np.ones(t_hold) * sustain_height
-        release = np.linspace(sustain_height, 0, t_release)
-        return np.concatenate((attack, decay, hold, release), 0)
+        res = []
+        for i in desired_indices:
+            attack = np.linspace(0, 1, t_attack[i,:])
+            decay = np.linspace(1, sustain_height, t_decay[i,:])
+            hold = np.ones(t_hold[i,:]) * sustain_height[i,:]
+            release = np.linspace(sustain_height[i,:], 0, t_release[i,:])
+            envelope = np.concatenate((attack, decay, hold, release), 0)
+            res.append(envelope)
+        return res
 
 
 class Track(NamedTuple):
     name: str
     pattern: List[int]
     note_values: float
-    shape_gen: Callable  # regular function or Module
+    envelope_gen: Callable  # regular function or Module
 
 
 class DrumMachine(Module):
@@ -45,7 +55,8 @@ class DrumMachine(Module):
     Trigger patterns go over any number of bars and repeat forever.
     The trigger patterns live in bar-space, not time:
     The trigger patterns will be spaced out in time according to the DrumMachine's bpm.
-    A trigger has no length! The length of a shape is the shape generator's concern.
+    A trigger has no length! The length of an envelope is the envelope generator's concern.
+    Combining overlapping envelopes is the TriggerModulator's concern.
     Ways to write down a trigger pattern:
         Direct:     [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0], (1 bar) or (1/16) or (2 bars) etc
                     Needs additional information: Either how many bars this is, or the note length.
@@ -55,7 +66,8 @@ class DrumMachine(Module):
                     [2,2] -> [1,0,1,0]
                     Downside: How to do offbeats, or other patterns that don't start at 1?
         ...?
-    Every track needs a shape generator
+    Currently supported: Direct with note_value, see Track class
+    Every track also has a envelope generator.
     """
 
     def __init__(self, bpm: Module, tracks: List[Track]):
@@ -94,7 +106,7 @@ class DrumMachine(Module):
 
         # now these trigger_tracks must be given shapes. this is an operation with time-context, and should be
         # handled by a pro - that is a module which deals with things like last_generated_signal or future_cache etc.
-
+        
 
         return None
 
@@ -112,16 +124,34 @@ class DrumMachine(Module):
         return triggers
 
 
+class TriggerModulator:
+    """
+        Put a shape on every trigger. If result is longer than a frame, keep the rest for the next call.
+        Combine overlaps with a suitable function: max, fst, snd, add, ...
+        """
+    def __init__(self):
+
+    def __call__(self, clock_signal: ClockSignal, triggers, shape_gen):
+        """Generate only one shape per trigger"""
+        trigger_indices = np.nonzero(triggers)
+                trigger_ts = clock_signal.ts[trigger_indices]
+                trigger_clock_indices = clock_signal.sample_indices[trigger_indices]
+        # the shape_gen should depend on trigger_ts and trigger_clock_indices only
+        shapes = []
+        for ts, clk_ind in zip (trigger_ts, trigger_clock_indices):
+                    shapes.append(shape_gen(ts, clk_ind))
+
+
 kick = Track(name="kick",
-                     pattern=[1, 0, 0, 1, 1, 0, 0, 1],
-                     note_values=1 / 8,
-                     shape_gen=ADSRShapeGen(attack=P(200), decay=P(10), sustain=P(0.5), release=P(200), hold=P(200)),
-                     )
+             pattern=[1, 0, 0, 1, 1, 0, 0, 1],
+             note_values=1 / 8,
+             shape_gen=ADSREnvelopeGen(attack=P(200), decay=P(10), sustain=P(0.5), release=P(200), hold=P(200)),
+             )
 snare = Track(name="snare",
-                      pattern=[0, 1, 0, 1],
-                      note_values=1 / 4,
-                      shape_gen=ADSRShapeGen(attack=P(100), decay=P(10), sustain=P(0.2), release=P(100), hold=P(50)),
-                      )
+              pattern=[0, 1, 0, 1],
+              note_values=1 / 4,
+              shape_gen=ADSREnvelopeGen(attack=P(100), decay=P(10), sustain=P(0.2), release=P(100), hold=P(50)),
+              )
 
 # t1 = DrumMachine._track_to_triggers(kick, 4)
 # t2 = DrumMachine._track_to_triggers(snare, 4)
@@ -135,17 +165,17 @@ class Drummin(Module):
         kick = Track(name="kick",
                      pattern=[1, 0, 0, 1, 1, 0, 0, 0],
                      note_values=1 / 8,
-                     shape_gen=ADSRShapeGen(attack=P(200), decay=P(10), sustain=P(0.5), release=P(200), hold=P(200)),
+                     shape_gen=ADSREnvelopeGen(attack=P(200), decay=P(10), sustain=P(0.5), release=P(200), hold=P(200)),
                      )
         snare = Track(name="snare",
                       pattern=[0, 1, 0, 1],
                       note_values=1 / 4,
-                      shape_gen=ADSRShapeGen(attack=P(100), decay=P(10), sustain=P(0.2), release=P(100), hold=P(50)),
+                      shape_gen=ADSREnvelopeGen(attack=P(100), decay=P(10), sustain=P(0.2), release=P(100), hold=P(50)),
                       )
         hihat = Track(name="hihat",
                       pattern=[1, 1, 1, 1, 1, 1, 1, 1],
                       note_values=1 / 8,
-                      shape_gen=ADSRShapeGen(attack=P(50), decay=P(10), sustain=P(0.1), release=P(100), hold=P(30)),
+                      shape_gen=ADSREnvelopeGen(attack=P(50), decay=P(10), sustain=P(0.1), release=P(100), hold=P(30)),
                       )
         self.output = DrumMachine(bpm=Parameter(44000, key='q'), tracks=[kick, snare, hihat])
         self.out = self.output
