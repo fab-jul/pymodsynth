758b6fc73e665a95c4db6fe3264afc9d5e03a2d8
fab-jul <fmentzer@me.com>
2021-12-17 17:31:54 +0100

    Reparameterize `ADSREnvelopeGenerator` sucht that the total_length can easily be adapted.

--- playground_v2/mz/envelopes.py
+++ playground_v2/mz/envelopes.py
@@ -6,25 +6,31 @@ from mz import helpers
 @helpers.mark_for_testing()
 class ADSREnvelopeGenerator(base.BaseModule):
 
-    attack: base.SingleValueModule = base.Constant(100.)
-    decay: base.SingleValueModule = base.Constant(100.)
+    # These times are relative to `total_length`.
+    attack: base.SingleValueModule = base.Constant(1.)
+    decay: base.SingleValueModule = base.Constant(1.)
     sustain: base.SingleValueModule = base.Constant(0.5)
-    release: base.SingleValueModule = base.Constant(100.)
-    hold: base.SingleValueModule = base.Constant(500.)
+    release: base.SingleValueModule = base.Constant(1.)
+    hold: base.SingleValueModule = base.Constant(5.)
+    total_length: base.SingleValueModule = base.Constant(1000.)
 
     def out_given_inputs(self, clock_signal: base.ClockSignal, 
-                         attack: float, decay: float, sustain: float, release: float, hold: float):
-        attack = np.linspace(0, 1, round(attack))
-        decay = np.linspace(1, sustain, round(decay))
-        hold = np.ones(round(hold)) * sustain
-        release = np.linspace(sustain, 0, round(release))
+                         attack: float, decay: float, sustain: float, release: float, hold: float,
+                         total_length: float):
+        """Return an envelope of length `total_length`"""
+        # TODO: Support int SingleValueModules in the framework?
+        total_length: int = round(total_length)
+        current_length = attack + decay + hold + release
+        scale = total_length / current_length
+        attack = np.linspace(0, 1, round(attack * scale))
+        decay = np.linspace(1, sustain, round(decay * scale))
+        hold = np.ones(round(hold * scale)) * sustain
+        release = np.linspace(sustain, 0, round(release * scale))
         result = np.concatenate((attack, decay, hold, release), 0)
-        # NOTE/describe: ignores clock_signal
+        # Fix off-by-one errors due to rounding, this will add/remove 1 frame to fit `total_length`.
+        if len(result) > total_length:
+            result = result[:total_length]
+        else:
+            missing = np.zeros((total_length - len(result),), dtype=result.dtype)
+            result = np.concatenate((result, missing), 0)
         return clock_signal.add_channel_dim(result)
-
-        if len(result) < clock_signal.num_samples:
-            result = np.concatenate((result, np.zeros(clock_signal.num_samples - len(result))), 0)
-        elif len(result) > clock_signal.num_samples:
-            print("WARN: envelope too big for clock_signal!", file=sys.stderr)
-            result = result[:clock_signal.num_samples]
-        return np.broadcast_to(result.reshape(-1, 1), clock_signal.shape)
